ac53ce3189bd8d725cd8090be7dd93461d5fde60
Ade Hery Shopyan <adeheryshopyan@gmail.com>
2022-02-06 22:39:25 +0700

    fix: fetch data service

--- src/hooks/service.ts
+++ src/hooks/service.ts
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
-import { fetchData } from 'utils/fetch-data'
+// import { fetchData } from 'utils/fetch-data'
+import axios from 'axios'
 
 const useService = () => {
   const [services, setServices] = useState<any[]>([])
@@ -7,11 +8,12 @@ const useService = () => {
   const getServiceList = useCallback(async () => {
     console.log('process.env.API_HOST,', process.env.API_HOST,)
     try {
-      const response = await fetchData({
-        apiHost: process.env.API_HOST,
-        url: '/services?populate=image',
-        method: 'GET'
-      })
+      // const response = await fetchData({
+      //   apiHost: process.env.API_HOST,
+      //   url: '/services?populate=image',
+      //   method: 'GET'
+      // })
+      const response = await axios.get('https://api-akunify.backpackr.xyz/api/services?populate=image');
 
       setServices(response?.data)
     } catch (error) {

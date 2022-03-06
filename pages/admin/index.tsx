3faac8b13a2438f09738a4b2b0a1e56ada096b3b
Andrian Robby Pratama <arprobi@gmail.com>
2022-03-06 15:19:22 +0700

    chore: change basic auth method

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,20 +1,9 @@
-import { NextPageContext } from 'next'
-import basicAuthMiddleware from 'nextjs-basic-auth-middleware'
+import React from 'react'
 
 import AdminContainer from 'containers/Admin'
 
-const AdminPage = (): JSX.Element => {
+const AdminPage: React.FC = () => {
   return <AdminContainer />
-}
-
-AdminPage.getInitialProps = async (ctx: NextPageContext) => {
-  const { req, res } = ctx
-
-  await basicAuthMiddleware(req as any, res as any)
-
-  return {
-    props: {},
-  }
 }
 
 export default AdminPage

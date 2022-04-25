b6ea5d94af4bf5dd6886a6c74c8d01a0162790aa
Andrian Robby Pratama <arprobi@gmail.com>
2022-04-25 13:59:38 +0700

    fix: fix json ld in sergice detail

--- src/components/Home/Banner/index.tsx
+++ src/components/Home/Banner/index.tsx
@@ -46,8 +46,8 @@ const Banner: React.FC = () => {
           <div className="w-full lg:w-1/2">
             <div className="header_hero_content pt-150 lg:pt-0">
               <h1 className="hero_title text-2xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-extrabold">
-                Sharing Subscription Services,{' '}
-                <span className="text-turquoise-50">Akunify</span>
+                Situs Patungan Akun Premium Murah, Aman, Praktis & Terpercaya
+                {/* <span className="text-turquoise-50">Akunify</span> */}
               </h1>
               <p className="mt-8 lg:mr-8">
                 Yuk! berlangganan layanan premium seperti Netflix, Youtube dan
--- src/containers/Subscribe/method.tsx
+++ src/containers/Subscribe/method.tsx
@@ -5,100 +5,100 @@ import { getImageUrl } from 'utils/index'
 
 const date = dayjs().add(1, 'month').format('YYYY-MM-DD')
 
-const ServiceDetailMeta = ({ service }: { service: any }): JSX.Element => (
-  <Meta
-    pageTitle={
-      'Patungan Akun - ' +
-      service?.attributes?.name +
-      ' Murah, Aman, Praktis & Terpercaya'
-    }
-    pageDescription={service?.attributes?.short_description}
-    og={{
-      type: 'product',
-      title: `Patungan ${service?.attributes?.name} dengan Akunify.com`,
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${service?.id}/${service?.attributes?.short_url}`,
-      description: service?.attributes?.short_description,
-      image: getImageUrl(service?.attributes?.image),
-    }}
-    author="Akunify.com"
-    fb={{
-      availability: 'in stock',
-      condition: 'new',
-      priceAmount: `${service?.attributes?.price}`,
-      priceCurrency: 'Rp',
-    }}
-    twitter={{
-      card: 'product',
-      site: '@kitabisacom',
-      title: `${service?.attributes?.name}`,
-      description: `${service?.attributes?.short_description}`,
-      image: `${getImageUrl(service?.attributes?.image)}`,
-      label1: 'product',
-      data1: `IDR${service?.attributes?.price}`,
-      label2: 'category',
-      data2: `Services`,
-    }}
-    content={{
-      title: service?.attributes?.name,
-      keywords: service?.attributes?.keywords,
-      author: 'Akunify.com',
-      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${service?.id}/${service?.attributes?.short_url}`,
-      alternate: `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${service?.id}/${service?.attributes?.short_url}`,
-    }}
-    structuredData={[
-      `{
-        "@context": "https://schema.org/",
-        "@type": "http://schema.org/Product",
-        "name": "${service?.attributes?.name}",
-        "description": "${service?.attributes?.short_description}",
-        "image": "${getImageUrl(service?.attributes?.image)}",
-        "url": "${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${service?.id}/${
-        service?.attributes?.short_url
-      }",
-        "sku": "${service?.id}-${service?.attributes?.short_url},"
-        "mpn": "${service?.id}",
-        "brand": {
-          "@type": "Brand",
-          "name": "Akunify"
-        },
-        "review": {
-          "@type": "Review",
-          "reviewRating": {
-            "@type": "Rating",
-            "ratingValue": "4",
-            "bestRating": "5"
+const ServiceDetailMeta = ({ service }: { service: any }): JSX.Element => {
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${service?.id}/${service?.attributes?.short_url}`
+
+  return (
+    <Meta
+      pageTitle={
+        'Patungan Akun ' +
+        service?.attributes?.name +
+        ' Murah, Aman, Praktis & Terpercaya'
+      }
+      pageDescription={service?.attributes?.short_description}
+      og={{
+        type: 'product',
+        title: `Patungan ${service?.attributes?.name} dengan Akunify.com`,
+        url: url,
+        description: service?.attributes?.short_description,
+        image: getImageUrl(service?.attributes?.image),
+      }}
+      author="Akunify.com"
+      fb={{
+        availability: 'in stock',
+        condition: 'new',
+        priceAmount: `${service?.attributes?.price}`,
+        priceCurrency: 'Rp',
+      }}
+      twitter={{
+        card: 'product',
+        site: '@kitabisacom',
+        title: `${service?.attributes?.name}`,
+        description: `${service?.attributes?.short_description}`,
+        image: `${getImageUrl(service?.attributes?.image)}`,
+        label1: 'product',
+        data1: `IDR${service?.attributes?.price}`,
+        label2: 'category',
+        data2: `Services`,
+      }}
+      content={{
+        title: service?.attributes?.name,
+        keywords: service?.attributes?.keywords,
+        author: 'Akunify.com',
+        canonical: url,
+        alternate: url,
+      }}
+      structuredData={[
+        `{
+          "@context": "https://schema.org/",
+          "@type": "http://schema.org/Product",
+          "name": "${service?.attributes?.name}",
+          "description": "${service?.attributes?.short_description}",
+          "image": "${getImageUrl(service?.attributes?.image)}",
+          "url": "${url}",
+          "sku": "${service?.id}-${service?.attributes?.short_url}",
+          "mpn": "${service?.id}",
+          "brand": {
+            "@type": "Brand",
+            "name": "Akunify"
           },
-          "author": {
-            "@type": "Person",
-            "name": "Fred Benson"
+          "review": {
+            "@type": "Review",
+            "reviewRating": {
+              "@type": "Rating",
+              "ratingValue": "4",
+              "bestRating": "5"
+            },
+            "author": {
+              "@type": "Person",
+              "name": "Fred Benson"
+            }
+          },
+          "offers": {
+            "@type": "http://schema.org/Offer",
+            "url": "${url}",
+            "availability": "http://schema.org/InStock",
+            "priceCurrency": "Rp",
+            "priceValidUntil": "${date}",
+            "price": "${service?.attributes?.price}"
+          },
+          "aggregateRating": {
+            "@type": "http://schema.org/AggregateRating",
+            "worstRating": 1,
+            "bestRating": 5,
+            "ratingValue": 4.9,
+            "reviewCount": 17409
           }
-        },
-        "offers": {
-          "@type": "http://schema.org/Offer",
-          "url": "${process.env.NEXT_PUBLIC_BASE_URL}/subscribe/${
-        service?.id
-      }/${service?.attributes?.short_url}",
-          "availability": "http://schema.org/InStock",
-          "priceCurrency": "Rp",
-          "priceValidUntil": "${date}",
-          "price": "${service?.attributes?.price}"
-        },
-        "aggregateRating": {
-          "@type": "http://schema.org/AggregateRating",
-          "worstRating": 1,
-          "bestRating": 5,
-          "ratingValue": 4.9,
-          "reviewCount": 17409
-        }
-      }`,
-      `{
-        "@context": "http://schema.org",
-        "@type": "Store",
-        "name": "Akunify.com",
-        "image": "${process.env.NEXT_PUBLIC_BASE_URL}/images/og-images.png"
-      }`,
-    ]}
-  />
-)
+        }`,
+        `{
+          "@context": "http://schema.org",
+          "@type": "Store",
+          "name": "Akunify.com",
+          "image": "${process.env.NEXT_PUBLIC_BASE_URL}/images/og-images.png"
+        }`,
+      ]}
+    />
+  )
+}
 
 export { ServiceDetailMeta }
